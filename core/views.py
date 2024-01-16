4ce31021c20c0d536c867298a8e67a86a24cdf85
tiwarimanvi <manvitiwari029@gmail.com>
2024-01-17 01:32:42 +0530

    the product detail page and sign up page and functionality

--- core/views.py
+++ core/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from items.models import Category, Item
+from .forms import SignupForm
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    print('Items:', items)
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items
+    })
+
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
--- items/views.py
+++ items/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import *
 # Create your views here.
+def detail(request, pk):
+    items = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=items.category, is_sold=False).exclude(pk=pk)[0:3]
+
+    return render(request, 'items/detail.html', {
+        'items': items,
+        'related_items': related_items
+    })
